c6ef8ad71c59f00eb0e16ed5fa5d411314bcd1c3
Justine McMillan <mcmillanjustine@gmail.com>
2023-01-23 13:49:29 -0400

    Updated CandyFloss bedframe data to most recent files (with no flagging at L2)

--- src_matlab/calc_ATOMIX_levels.m
+++ src_matlab/calc_ATOMIX_levels.m
@@ -12,7 +12,7 @@ addpath('functions')
 
 %% Select dataset and processing ID
 % Select one or more files to process
-caseIDs = {'4E'};
+caseIDs = {'2A','2B','2C','2D'};
 % caseIDs = {'1A','1B','1C','1D','2A','2B','2C','2D','3A','3B','3C','4A','4B','4C','4D','4E'}; %ALL
 
 %% Process
